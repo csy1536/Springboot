aecc4bb101d3216185119e8a3c5d691a8c564c66
sychoi <coalla486@naver.com>
2023-09-17 18:30:29 +0900

    RestTemplate 커스텀 추가

--- src/main/java/com/springboot/jpa/service/RestTemplateService.java
+++ src/main/java/com/springboot/jpa/service/RestTemplateService.java
@@ -2,10 +2,15 @@ package com.springboot.jpa.service;
 
 import java.net.URI;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -30,6 +35,7 @@ public class RestTemplateService {
 		
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+		ResponseEntity<String> responseEntity2 = restTemplate.getForEntity(uri, String.class);
 		
 		logger.info("[request uri] ::: {}", uri);
 		
@@ -82,12 +88,35 @@ public class RestTemplateService {
 															.post(uri)
 															.header("my-header", "post api test")
 															.body(dto);
-		
-		RestTemplate restTemplate = new RestTemplate();
+		RestTemplate restTemplate = restTemplate();//new RestTemplate();
 		ResponseEntity<ProductResponseDTO> responseEntity = restTemplate.exchange(requestEntity, ProductResponseDTO.class);
 		
 		logger.info("[request uri] ::: {}", uri);
 		
 		return responseEntity;
+	}
+	
+	public RestTemplate restTemplate() {
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		
+		//[방법1]
+		HttpClient client = HttpClientBuilder.create()
+								.setMaxConnTotal(500)
+								.setMaxConnPerRoute(500)
+								.build();
+		
+		//[방법2]
+		CloseableHttpClient httpClient = HttpClients.custom()
+											.setMaxConnTotal(500)
+											.setMaxConnPerRoute(500)
+											.build();
+		
+		factory.setHttpClient(httpClient); //HttpClient와 CloseableHttpClient의 차이
+		factory.setConnectTimeout(2000);
+		factory.setReadTimeout(5000);
+		
+		RestTemplate restTemplate = new RestTemplate(factory);
+		
+		return restTemplate;
 	}
 }
